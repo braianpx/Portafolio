6d0bf4b61a0301e4563edb119b4063361adb091b
braianpx <braivaldez16@gmail.com>
2023-03-08 20:47:18 -0300

    fix bugs

--- src/components/home/SkillsBar.jsx
+++ src/components/home/SkillsBar.jsx
@@ -5,11 +5,12 @@ function SkillsBar({ skill, img }) {
   return (
     <div 
     className="mb-4"
-    style={{ width: "95%", height:'15%', display:'flex', justifyContent:'center', alignItems:'center'}}>
-      <p className="lead mb-1 mt-2 me-3">{skill}</p>
+    style={{ width: "95%", display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <p className="lead mb-1 mt-2 me-3 text-center text">{skill}</p>
           {img && 
-          <div style={{width:'25%', height:'50%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <div style={{width:'20%', height:'100%', display:'flex', justifyContent:'center', alignItems:'center'}}>
           <Image
+          height={'100%'}
           width={'100%'}
           style={{filter:'brightness(1.1)',mixBlendMode:'multiply'}}
           src={img}
--- src/components/home/SkillsTab.jsx
+++ src/components/home/SkillsTab.jsx
@@ -19,22 +19,22 @@ function SkillsSection({ skills }) {
 function SkillsTab({ skills }) {
   return (
     <>
-      <Col xs={12} md={3}>
+      <Col xs={12} sm={6} xl={3}>
         <SkillsSection
           skills={skills.slice(0, Math.floor(skills.length / 4))}
         />
       </Col>
-      <Col xs={12} md={3}>
+      <Col xs={12} sm={6} xl={3}>
         <SkillsSection
           skills={skills.slice(Math.floor(skills.length / 4), Math.floor(skills.length / 2))}
         />
       </Col>
-      <Col xs={12} md={3}>
+      <Col xs={12} sm={6} xl={3}>
         <SkillsSection
           skills={skills.slice(Math.floor(skills.length / 2), (Math.floor(skills.length / 4) + Math.floor(skills.length / 2)))}
         />
       </Col>
-      <Col xs={12} md={3}>
+      <Col xs={12} sm={6} xl={3}>
         <SkillsSection
           skills={skills.slice((Math.floor(skills.length / 4) + Math.floor(skills.length / 2)), skills.length)}
         />
