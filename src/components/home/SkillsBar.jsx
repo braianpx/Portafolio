c7b937253fff340ba78c384f52f06756e7ee002d
braianpx <braivaldez16@gmail.com>
2023-03-08 19:17:55 -0300

    modify habilities

--- src/components/home/SkillsBar.jsx
+++ src/components/home/SkillsBar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import ProgressBar from "react-bootstrap/ProgressBar";
+import Image from 'react-bootstrap/Image';
 
-function SkillsBar({ skill, value, isScrolled }) {
+function SkillsBar({ skill, img }) {
   return (
-    <div style={{ width: "95%" }}>
-      <p className="lead mb-1 mt-2">{skill}</p>
-      <ProgressBar
-        className={!isScrolled ? "progress" : " progress-bar-animation"}
-        now={value}
-      />
+    <div 
+    className="mb-4"
+    style={{ width: "95%", height:'15%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <p className="lead mb-1 mt-2 me-3">{skill}</p>
+          {img && 
+          <div style={{width:'25%', height:'50%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <Image
+          width={'100%'}
+          style={{filter:'brightness(1.1)',mixBlendMode:'multiply'}}
+          src={img}
+          />      
+          </div>
+          }
     </div>
   );
 }
