436f9e2eb426c7f2dcabae4eb5a535a2db681e60
braianpx <braivaldez16@gmail.com>
2026-01-27 01:51:18 -0300

    update config frame option

--- src/utils/FrameMotion.js
+++ src/utils/FrameMotion.js
@@ -26,3 +26,12 @@ export const filterVariants = {
       },
     },
   };
+
+export const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { 
+      opacity: 1, 
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" }
+    },
+  };
